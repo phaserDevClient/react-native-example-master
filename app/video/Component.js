d18f0607cbb15064bf9dcd47ff2181c67198cd39
ha <ha@ecorau.com>
2018-10-29 10:37:14 +0700

    youtube video
    - user web view to view youtube video

--- app/video/Component.js
+++ app/video/Component.js
@@ -1,21 +1,61 @@
 import React, {Component} from 'react';
 import {Video} from 'expo';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, WebView, Dimensions} from 'react-native';
 
+const windowWidth = Dimensions.get('window');
 export default class VideoComponent extends Component {
+    renderVideo() {
+        return (
+            <Video
+                source={require('../assets/videotest.mp4')}
+                // source={{uri:'https://www.youtube.com/watch?v=M-P4QBt-FWw'}}
+                rate={1.0}
+                volume={1.0}
+                isMuted={false}
+                shouldPlay
+                isLooping
+                style={styles.backgroundVideo}
+            />
+        );
+    }
+
+    // renderYouTubeVideo() {
+    //     return (
+    //         <YouTube
+    //             videoId="M-P4QBt-FWw"   // The YouTube video ID
+    //             play={true}             // control playback of video with true/false
+    //             fullscreen={true}       // control whether the video should play in fullscreen or inline
+    //             loop={true}
+    //         />
+    //     );
+    // }
+
+    renderYouTubeVideo() {
+        return (
+            <WebView
+                style={styles.videoContainer}
+                javaScriptEnabled={true}
+                allowsInlineMediaPlayback={true}
+                originWhitelist={['*']}
+                // source={{
+                //     uri: 'https://www.youtube.com/embed/cBVGlBWQzuc?autoplay=1'
+                // }}
+                source={{
+                    html:"<html><body><iframe style='align-content: center; align-items: center; width: 560px; height: 315px; position: relative' src=\"https://www.youtube.com/embed/P_C0HdWlvZA?autoplay=0\" frameborder=\"0\" allow=\"autoplay; encrypted-media\"></iframe></body></html>"
+                }}
+            />
+        );
+    }
+
     render() {
         return (
             <View>
-                <Video
-                    source={require('../assets/videotest.mp4')}
-                    // source={{uri:'https://www.youtube.com/watch?v=M-P4QBt-FWw'}}
-                    rate={1.0}
-                    volume={1.0}
-                    isMuted={false}
-                    shouldPlay
-                    isLooping
-                    style={styles.backgroundVideo}
-                />
+                <View>
+                    {this.renderVideo()}
+                </View>
+                <View>
+                    {this.renderYouTubeVideo()}
+                </View>
             </View>
         );
     }
@@ -23,14 +63,13 @@ export default class VideoComponent extends Component {
 
 var styles = StyleSheet.create({
     backgroundVideo: {
-        width: 300,
+        width: windowWidth.width,
         height: 300,
         position: 'absolute',
         top: 0,
         left: 0,
         bottom: 0,
         right: 0,
-        justifyContent: 'center'
     },
     container: {
         flex: 1,
@@ -39,4 +78,9 @@ var styles = StyleSheet.create({
         paddingTop: 10,
         backgroundColor: '#ecf0f1',
     },
+    videoContainer: {
+        paddingTop: 315,
+        top: 315,
+        backgroundColor: '#ecf0f1',
+    }
 });
